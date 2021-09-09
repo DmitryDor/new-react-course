1c355375dcae29dee79810cd3769df3c2809941d
Your NameDmitryDor <you@example.com>
2021-09-09 12:15:06 +0300

    4 controlled and uncontrolled inputs

--- src/App.js
+++ src/App.js
@@ -1,23 +1,53 @@
 import './App.css';
 import './styles/app.css'
 import {PostList} from "./components/PostList";
-import {useState} from "react";
+import { useState} from "react";
+import {MyButton} from "./components/UI/button/MyButton";
+import {MyInput} from "./components/UI/input/MyInput";
 
 export function App() {
     const [posts, setPosts] = useState([
         {id: 1, title: "JS", body: "Description1"},
         {id: 2, title: "HTML", body: "Description2"},
         {id: 3, title: "CSS", body: "Description3"},
-        {id: 4, title: "React", body: "Description4"}
+
     ])
+
+    // const [title, setTitle] = useState('')
+    // const [body, setBody] = useState('')
+
+    const [post, setPost] = useState({title: '', body: ''})
+
+    const onChangeTitleHandler = (e) => {
+        const newTitle = e.currentTarget.value
+        setPost({...post, title: newTitle})
+    }
+    const onChangeBodyHandler = (e) => {
+        const newBody = e.currentTarget.value
+        setPost({...post,body: newBody})
+    }
+
+    const addNewPost = (e) => {
+        e.preventDefault()
+        setPosts([ ...posts, {...post, id: Date.now()}])
+        setPost({title: '', body: ''})
+    }
 
 
     return (
         <div className="App">
             <form >
-                <input type="text" placeholder="Название поста"/>
-                <input type="text" placeholder="Описание поста"/>
-                <button>Создать пост</button>
+                {/*Управляемый инпут*/}
+                <MyInput placeholder = "Название поста" value={post.title} onChange={onChangeTitleHandler}/>
+
+                {/*не управляемый инпут получаем доступ напрямую к DOM*/}
+                {/*<input ref={bodyInputRef}  type="text"/>*/}
+
+                {/*не управляемый или не конролируемый инпут получаем доступ напрямую к DOM*/}
+                {/*<MyInput placeholder="Описание поста" ref={bodyInputRef}/>*/}
+                <MyInput placeholder = "Описание поста" value={post.body} onChange={onChangeBodyHandler}/>
+                <MyButton onClick={addNewPost} >Создать пост</MyButton>
+
             </form>
             <PostList posts={posts} title="Список постов 1"/>
         </div>
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -9,8 +9,8 @@ export const PostList = (props) => {
     return (
         <div >
             <h1 style={{textAlign: "center"}}>{props.title}</h1>
-            {props.posts.map(post =>
-                <PostItem  post={post} key={post.id}/>
+            {props.posts.map((post, index) =>
+                <PostItem  post={post} key={post.id} number={index + 1}/>
             )}
         </div>
     );
